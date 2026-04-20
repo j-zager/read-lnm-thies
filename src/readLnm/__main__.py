6c2b008d63de27da3fa2d838559e8e125c207bcf
V5jz FZ-55 <272808957+j-zager@users.noreply.github.com>
2026-04-20 14:52:11 +0200

    inital commit: readLnm

---
+++ src/readLnm/__main__.py
@@ -0,0 +1,4 @@
+from readLnm.communicateLnm import communicate
+
+if __name__ == "__main__":
+    communicate()
---
+++ src/readLnm/communicateLnm.py
@@ -0,0 +1,7 @@
+
+def communicate():
+    print(f"start LNM communication")
+
+
+if __name__ == "__main__":
+    communicate()
